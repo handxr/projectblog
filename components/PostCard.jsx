783fb5bfc5a01516c735f9b65c5e2df82a85e2a9
Juan <hander.juan@gmail.com>
2021-11-06 12:41:12 +0100

    gogo

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -65,7 +65,7 @@ const PostCard = ({ post }) => {
       <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">{post.excerpt}</p>
       <div className="text-center">
         <Link href={`/post/${post.slug}`}>
-            <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-700 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">Continue reading</span>
+            <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-700 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">Continua Leyendo</span>
         </Link>
       </div>
     </div>
